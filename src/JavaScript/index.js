dd9618a8483931d0151f4faaae593b6808eb44d3
charlie retzler <charlie.retzler@gmail.com>
2020-08-17 00:02:54 -0400

    Fixed application running

--- src/JavaScript/index.js
+++ src/JavaScript/index.js
@@ -1,5 +1,5 @@
 // This index.js is only here in order to run a console app, if needed. Any code you implement should be in app.js instead.
-const { bestHand } = require('./app');
+const { bestHand } = require('./hand');
 
 let result = bestHand('KS 2H 2C JD TD');
 console.log(result);
