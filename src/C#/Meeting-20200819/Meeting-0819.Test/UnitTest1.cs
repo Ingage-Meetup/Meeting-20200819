1c9476025a09210bfae0f5212314f943df38fe1e
Jon Newell <loxmanCV67@gmail.com>
2020-08-19 20:34:44 -0400

    added PokerHand.cs with hand ranking up through three of a kind, with unit tests.

--- src/C#/Meeting-20200819/Meeting-0819.Test/UnitTest1.cs
+++ src/C#/Meeting-20200819/Meeting-0819.Test/UnitTest1.cs
@@ -1,8 +1,9 @@
 using NUnit.Framework;
+using Meeting_20200819;
 
 namespace Meeting_0819.Test
 {
-    public class Tests
+    public class Given_A_Set_Of_Cards
     {
         [SetUp]
         public void Setup()
@@ -10,9 +11,56 @@ namespace Meeting_0819.Test
         }
 
         [Test]
-        public void Test1()
+        public void When_It_Contains_5_Cards_Then_It_Is_A_Valid_Poker_Hand()
         {
-            Assert.Pass();
+            // Arrange
+            var pokerHand = new PokerHand("KS 2H 5C JD TD");
+
+            // Acting on Data
+            var valid = pokerHand.IsValid();
+
+            // Asserting 
+            Assert.IsTrue(valid);
+        }
+
+        [Test]
+        public void When_The_Hand_With_No_Rank_Then_Its_Rank_Is_High_Card()
+        {
+            var pokerHand = new PokerHand("KS 2H 5C JD TD");
+
+            var rank = pokerHand.RankHand();
+
+            Assert.IsTrue(rank == Rank.HighCard);
+        }
+
+        [Test]
+        public void When_The_Hand_Contains_Two_Cards_With_Fours_Then_Its_Rank_Is_A_Pair()
+        {
+            var pokerHand = new PokerHand("KS 4H 5C 4D TD");
+
+            var rank = pokerHand.RankHand();
+
+            Assert.IsTrue(rank == Rank.Pair);
+        }
+
+        [Test]
+        public void When_The_Hand_Contains_Two_Fours_And_Two_Kings_Then_Its_Rank_Is_Two_Pair()
+        {
+            var pokerHand = new PokerHand("KS 4H 5C 4D KD");
+
+            var rank = pokerHand.RankHand();
+
+            Assert.IsTrue(rank == Rank.TwoPair);
+        }
+
+        [Test]
+        public void When_The_Hand_Contains_Three_Fours_Then_Its_Rank_Is_Three_Of_A_Kind()
+        {
+            var pokerHand = new PokerHand("KS 4H 5C 4D 4C");
+
+            var rank = pokerHand.RankHand();
+
+            Assert.IsTrue(rank == Rank.ThreeOfAKind);
         }
     }
 }
---
+++ src/C#/Meeting-20200819/Meeting-20200819/PokerHand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Meeting_20200819
+{
+    public enum Rank
+    {
+        HighCard = 1,
+        Pair = 2,
+        TwoPair = 3,
+        ThreeOfAKind = 4
+    }
+
+    public class PokerHand
+    {
+        
+
+        public PokerHand(string hand)
+        {
+            cards = hand.Split(" ");            
+        }
+
+        public bool IsValid()
+        {            
+            return cards.Length == 5;
+        }
+
+        public Rank RankHand()
+        {
+            var numbers = new List<string>();
+            foreach (var card in cards)
+            {
+                numbers.Add(card.Substring(0, 1));
+            }
+
+            numbers.Sort();
+
+
+            int pairCount = NumberOfPairs(numbers);
+            bool isThreeOfKind = IsThreeOfKind(numbers);
+
+            if (isThreeOfKind) return Rank.ThreeOfAKind;
+            if (pairCount == 2) return Rank.TwoPair;
+            if (pairCount == 1) return Rank.Pair;
+
+            return Rank.HighCard;
+        }
+
+        private int NumberOfPairs(List<string> numbers)
+        {
+            var pairCount = 0;
+
+            for (var number = 0; number < numbers.Count - 1; number++)
+            {
+                if (numbers[number] == numbers[number + 1])
+                {
+                    pairCount++;
+                }
+            }
+
+            return pairCount;
+        }
+
+        private bool IsThreeOfKind(List<string> numbers)
+        {            
+            for (var number = 0; number < numbers.Count - 2; number++)
+            {
+                if(numbers[number] == numbers[number + 1] && numbers[number] == numbers[number + 2])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private string[] cards;
+       
+    }
+}
